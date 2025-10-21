a6f55a241e63bcf2f7da78a9a5903312bc040299
ViuRemix <viu106018@donga.edu.vn>
2025-10-21 11:11:20 +0700

    fix EnvConfig for Render

--- src/main/java/com/viusoftware/training/training_system/config/EnvConfig.java
+++ src/main/java/com/viusoftware/training/training_system/config/EnvConfig.java
@@ -1,3 +1,21 @@
+// package com.viusoftware.training.training_system.config;
+
+// import io.github.cdimascio.dotenv.Dotenv;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+// public class EnvConfig {
+//     @Bean
+//     public Dotenv dotenv() {
+//         return Dotenv.configure()
+//                 .directory("d:\\Hoc_spring_boot\\student_management\\training-system") // Đường dẫn đến thư mục chứa file .env
+//                 .load();
+//     }
+// }
+
+
+
 package com.viusoftware.training.training_system.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
@@ -6,10 +24,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EnvConfig {
+
     @Bean
     public Dotenv dotenv() {
         return Dotenv.configure()
-                .directory("d:\\Hoc_spring_boot\\student_management\\training-system") // Đường dẫn đến thư mục chứa file .env
+                .ignoreIfMissing() 
                 .load();
     }
 }
